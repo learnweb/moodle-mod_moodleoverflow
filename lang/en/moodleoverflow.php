4d6fa032e12093dbc6bb8659de86c1c4d4eda487
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2018-02-13 11:55:49 +0100

    Lang: Remove "moodleoverflow" from most user-facing strings

--- lang/en/moodleoverflow.php
+++ lang/en/moodleoverflow.php
@@ -30,15 +30,15 @@ defined('MOODLE_INTERNAL') || die();
 // Default strings.
 $string['modulename']             = 'Moodleoverflow';
 $string['modulenameplural']       = 'Moodleoverflows';
-$string['modulename_help']        = 'The moodleoverflow module enables participants to use a StackOverflow-like forumstructure.';
+$string['modulename_help']        = 'The Moodleoverflow module enables participants to use a question-answer forum structure. The forum display is non-chronological as the ordering depends on collaborative voting instead of on time.';
 $string['moodleoverflowfieldset'] = 'Custom example fieldset';
 $string['moodleoverflowname']     = 'Moodleoverflow name';
 $string['moodleoverflow']         = 'Moodleoverflow';
-$string['pluginadministration']   = 'moodleoverflow administration';
+$string['pluginadministration']   = 'Moodleoverflow administration';
 $string['pluginname']             = 'Moodleoverflow';
 
 // Capabilities.
-$string['moodleoverflow:addinstance']         = 'Add a new moodleoverflow instance';
+$string['moodleoverflow:addinstance']         = 'Add a new Moodleoverflow instance';
 $string['moodleoverflow:submit']              = 'Submit moodleoverflow';
 $string['moodleoverflow:allowforcesubscribe'] = 'Allow forced subscription';
 $string['moodleoverflow:createattachment']    = 'Create attachments';
@@ -54,15 +54,15 @@ $string['moodleoverflow:replypost']           = 'Reply in discussion';
 $string['moodleoverflow:viewdiscussion']      = 'View discussion';
 $string['moodleoverflow:view']                = 'View discussionlist';
 $string['moodleoverflow:marksolved']          = 'Mark a post as solved';
-$string['nowallsubscribed']                   = 'All moodleoverflows in {$a} are subscribed.';
-$string['nowallunsubscribed']                 = 'All moodleoverflows in {$a} are not subscribed.';
+$string['nowallsubscribed']                   = 'All forums in {$a} are subscribed.';
+$string['nowallunsubscribed']                 = 'All forums in {$a} are unsubscribed.';
 
 // Strings for the view.php.
-$string['noviewdiscussionspermission'] = 'You do not have the permission to view discussions in this moodleoverflow';
+$string['noviewdiscussionspermission'] = 'You do not have the permission to view discussions in this forum';
 
 // Strings for the locallib.php.
 $string['addanewdiscussion']    = 'Add a new discussion topic';
-$string['nodiscussions']        = 'There are no discussion topics yet in this moodleoverflow.';
+$string['nodiscussions']        = 'There are no discussion topics yet in this forum.';
 $string['headerdiscussion']     = 'Discussion';
 $string['headerstartedby']      = 'Started by';
 $string['headerreplies']        = 'Replies';
@@ -84,11 +84,11 @@ $string['deletesure']           = 'Are you sure you want to delete this post?';
 $string['deletesureplural']     = 'Are you sure you want to delete this post and all replies? ({$a} posts)';
 
 // Strings for the settings.php.
-$string['configmanydiscussions']     = 'Maximum number of discussions shown in a moodleoverflow instance per page';
+$string['configmanydiscussions']     = 'Maximum number of discussions shown in a Moodleoverflow instance per page';
 $string['manydiscussions']           = 'Discussions per page';
 $string['maxattachmentsize']         = 'Maximum attachment size';
-$string['maxattachmentsize_help']    = 'This setting specifies the largest size of file that can be attached to a moodleoverflow post.';
-$string['configmaxbytes']            = 'Default maximum size for all moodleoverflow attachments on the site (subject to course limits and other local settings)';
+$string['maxattachmentsize_help']    = 'This setting specifies the largest size of file that can be attached to a forum post.';
+$string['configmaxbytes']            = 'Default maximum size for all forum attachments on the site (subject to course limits and other local settings)';
 $string['maxattachments']            = 'Maximum number of attachments';
 $string['maxattachments_help']       = 'This setting specifies the maximum number of files that can be attached to a forum post.';
 $string['configmaxattachments']      = 'Default maximum number of attachments allowed per post.';
@@ -104,7 +104,7 @@ $string['configtrackingtype']        = 'Default setting for read tracking.';
 $string['trackmoodleoverflow']       = 'Track unread posts';
 $string['configtrackmoodleoverflow'] = 'Set to \'yes\' if you want to track read/unread for each user.';
 $string['forcedreadtracking']        = 'Allow forced read tracking';
-$string['configforcedreadtracking']  = 'Allows moodleoverflows to be set to forced read tracking. Will result in decreased performance for some users, particularly on courses with many moodleoverflows and posts. When off, any moodleoverflows previously set to Forced are treated as optional.';
+$string['configforcedreadtracking']  = 'Allows Moodleoverflows to be set to forced read tracking. Will result in decreased performance for some users, particularly on courses with many moodleoverflows and posts. When off, any moodleoverflows previously set to Forced are treated as optional.';
 $string['cleanreadtime']             = 'Mark old posts as read hour';
 $string['configcleanreadtime']       = 'The hour of the day to clean old posts from the \'read\' table.';
 
@@ -126,11 +126,11 @@ $string['maxmailingtime']              = 'Maximal mailing time';
 $string['configmaxmailingtime']        = 'Posts older than this number of hours will not be mailed to the users. This will help to avoid problems where the cron has not een running for a long time.';
 
 // Strings for the post.php.
-$string['invalidmoodleoverflowid'] = 'Moodleoverflow ID was incorrect';
+$string['invalidmoodleoverflowid'] = 'Forum ID was incorrect';
 $string['invalidparentpostid']     = 'Parent post ID was incorrect';
 $string['notpartofdiscussion']     = 'This post is not part of a discussion!';
 $string['noguestpost']             = 'Sorry, guests are not allowed to post.';
-$string['nopostmoodleoverflow']    = 'Sorry, you are not allowed to post to this moodleoverflow.';
+$string['nopostmoodleoverflow']    = 'Sorry, you are not allowed to post to this forum.';
 $string['yourreply']               = 'Your reply';
 $string['re']                      = 'Re:';
 $string['invalidpostid']           = 'Invalid post ID - {$a}';
@@ -158,14 +158,14 @@ $string['replyfirst']                  = 'Answer';
 $string['message']                     = 'Message';
 $string['discussionsubscription']      = 'Discussion subscription';
 $string['discussionsubscription_help'] = 'Subscribing to a discussion means you will receive notifications of new posts to that discussion.';
-$string['posttomoodleoverflow']        = 'Post to moodleoverflow';
+$string['posttomoodleoverflow']        = 'Post to forum';
 $string['erroremptysubject']           = 'Post subject cannot be empty.';
 $string['erroremptymessage']           = 'Post message cannot be empty';
 $string['yournewtopic']                = 'Your new discussion topic';
 
 // Strings for the classes/ratings.php.
 $string['postnotexist']             = 'Requested post does not exist';
-$string['noratemoodleoverflow']     = 'Sorry, you are not allowed to vote in this moodleoverflow.';
+$string['noratemoodleoverflow']     = 'Sorry, you are not allowed to vote in this forum.';
 $string['configallowratingchange']  = 'Can a user change its ratings?';
 $string['allowratingchange']        = 'Allow rating changes';
 $string['configpreferteachersmark'] = 'The answer marked as solved by a teacher are prioritized over the answer marked as helpful by the starter of the discussion.';
@@ -181,7 +181,7 @@ $string['invaliddiscussionid']         = 'Discussion ID was incorrect';
 $string['notexists']                   = 'Discussion no longer exists';
 $string['discussionname']              = 'Discussion name';
 $string['discussionlocked']            = 'This discussion has been locked so you can no longer reply to it.';
-$string['hiddenmoodleoverflowpost']    = 'Hidden moodleoverflow post';
+$string['hiddenmoodleoverflowpost']    = 'Hidden forum post';
 $string['moodleoverflowsubjecthidden'] = 'Subject (hidden)';
 $string['moodleoverflowauthorhidden']  = 'Author (hidden)';
 $string['moodleoverflowbodyhidden']    = 'This post cannot be viewed by you, probably because you have not posted in the discussion, the maximum editing time hasn\'t passed yet, the discussion has not started or the discussion has expired.';
@@ -204,21 +204,20 @@ $string['unknownerror']                 = 'This is not expected to happen.';
 $string['crontask']                     = 'Moodleoverflow maintenance jobs';
 $string['taskcleanreadrecords']         = 'Moodleoverflow maintenance job to clean old read records';
 $string['tasksendmails']                = 'Moodleoverflow maintenance job to send mails';
-$string['nopermissiontosubscribe']      = 'You do not have the permission to view moodleoverflow subscribers';
-$string['subscribeenrolledonly']        = 'Sorry, only enrolled users are allowed to subscribe to moodleoverflow post notifications.';
+$string['nopermissiontosubscribe']      = 'You do not have the permission to view subscribers';
+$string['subscribeenrolledonly']        = 'Sorry, only enrolled users are allowed to subscribe to post notifications.';
 $string['everyonecannowchoose']         = 'Everyone can now choose to be subscribed';
-$string['everyoneisnowsubscribed']      = 'Everyone is now subscribed to this moodleoverflow';
 $string['noonecansubscribenow']         = 'Subscriptions are now disallowed';
 $string['invalidforcesubscribe']        = 'Invalid force subscription mode';
 $string['nownotsubscribed']             = '{$a->name} will NOT be notified of new posts in \'{$a->moodleoverflow}\'';
-$string['cannotunsubscribe']            = 'Could not unsubscribe you from that moodleoverflow';
+$string['cannotunsubscribe']            = 'Could not unsubscribe you from that forum';
 $string['discussionnownotsubscribed']   = '{$a->name} will NOT be notified of new posts in \'{$a->discussion}\' of \'{$a->moodleoverflow}\'';
 $string['disallowsubscribe']            = 'Subscriptions not allowed';
-$string['noviewdiscussionspermission']  = 'You do not have the permission to view discussions in this moodleoverflow';
+$string['noviewdiscussionspermission']  = 'You do not have the permission to view discussions in this forum';
 $string['nowsubscribed']                = '{$a->name} will be notified of new posts in \'{$a->moodleoverflow}\'';
 $string['discussionnowsubscribed']      = '{$a->name} will be notified of new posts in \'{$a->discussion}\' of \'{$a->moodleoverflow}\'';
-$string['unsubscribe']                  = 'Unsubscribe from this moodleoverflow';
-$string['subscribe']                    = 'Subscribe to this moodleoverflow';
+$string['unsubscribe']                  = 'Unsubscribe from this forum';
+$string['subscribe']                    = 'Subscribe to this forum';
 $string['confirmunsubscribediscussion'] = 'Do you really want to unsubscribe from discussion \'{$a->discussion}\' in moodleoverflow \'{$a->moodleoverflow}\'?';
 $string['confirmunsubscribe']           = 'Do you really want to unsubscribe from moodleoverflow \'{$a}\'?';
 $string['confirmsubscribediscussion']   = 'Do you really want to subscribe to discussion \'{$a->discussion}\' in forum \'{$a->moodleoverflow}\'?';
@@ -235,7 +234,7 @@ $string['postincontext']                = 'See this post in context';
 $string['unsubscribediscussion']        = 'Unsubscribe from this discussion';
 $string['nownottracking']               = '{$a->name} is no longer tracking \'{$a->moodleoverflow}\'.';
 $string['nowtracking']                  = '{$a->name} is now tracking \'{$a->moodleoverflow}\'.';
-$string['cannottrack']                  = 'Could not stop tracking that moodleoverflow';
+$string['cannottrack']                  = 'Could not stop tracking that forum';
 $string['notrackmoodleoverflow']        = 'Don\'t track unread posts';
 $string['trackmoodleoverflow']          = 'Track unread posts';
 $string['discussions']                  = 'Discussions';
@@ -244,14 +243,14 @@ $string['unreadposts']                  = 'Unread posts';
 $string['unreadpostsnumber']            = '{$a} unread posts';
 $string['unreadpostsone']               = '1 unread post';
 $string['tracking']                     = 'Track';
-$string['allsubscribe']                 = 'Subscribe to all moodleoverflows';
-$string['allunsubscribe']               = 'Unsubscribe from all moodleoverflows';
-$string['generalmoodleoverflows']       = 'Moodleoverflows in this course';
-$string['subscribestart']               = 'Send me notifications of new posts in this moodleoverflow';
-$string['subscribestop']                = 'I don\'t want to be notified of new posts in this moodleoverflow';
-$string['everyoneisnowsubscribed']      = 'Everyone is now subscribed to this moodleoverflow';
-$string['everyoneissubscribed']         = 'Everyone is subscribed to this moodleoverflow';
-$string['mailindexlink']                = 'Change your moodleoverflow preferences: {$a}';
+$string['allsubscribe']                 = 'Subscribe to all forums';
+$string['allunsubscribe']               = 'Unsubscribe from all forums';
+$string['generalmoodleoverflows']       = 'Forums in this course';
+$string['subscribestart']               = 'Send me notifications of new posts in this forum';
+$string['subscribestop']                = 'I don\'t want to be notified of new posts in this forum';
+$string['everyoneisnowsubscribed']      = 'Everyone is now subscribed to this forum';
+$string['everyoneissubscribed']         = 'Everyone is subscribed to this forum';
+$string['mailindexlink']                = 'Change your forum preferences: {$a}';
 $string['gotoindex']                    = 'Manage preferences';
 $string['areaattachment']               = 'Attachments';
 $string['areapost']                     = 'Messages';
@@ -277,7 +276,7 @@ $string['eventreadtrackingenabled']           = 'Read tracking enabled';
 
 $string['subscriptiontrackingheader']   = 'Subscription and tracking';
 $string['subscriptionmode']             = 'Subscription mode';
-$string['subscriptionmode_help']        = 'When a participant is subscribed to a moodleoverflow it means they will receive forum post notifications. There are 4 subscription mode options:
+$string['subscriptionmode_help']        = 'When a participant is subscribed to a forum it means they will receive forum post notifications. There are 4 subscription mode options:
 
 * Optional subscription - Participants can choose whether to be subscribed
 * Forced subscription - Everyone is subscribed and cannot unsubscribe
