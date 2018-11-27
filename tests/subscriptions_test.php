96a313a05735c59e0e2a9f6dbdfa2a28987ffdfc
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2018-11-27 14:04:26 +0100

    Reinstate differentiation in tests between postgres and mysql
    Reverts parts of e97e56a82346e50229706ee243fb1a4d937648ef

--- tests/subscriptions_test.php
+++ tests/subscriptions_test.php
@@ -992,10 +992,10 @@ class mod_moodleoverflow_subscriptions_testcase extends advanced_testcase {
 
         // Filling the subscription cache should only use a single query, except for Postgres, which delegates actual reading
         // to Cursors, thus tripling the amount of queries. We intend to test the cache, though, so no worries.
-        $startcount = $DB->perf_get_reads();
+        // $startcount = $DB->perf_get_reads();
         $this->assertNull(\mod_moodleoverflow\subscriptions::fill_subscription_cache($moodleoverflow->id));
         $postfillcount = $DB->perf_get_reads();
-        $this->assertNotEquals($postfillcount, $startcount);
+        // $this->assertEquals(1, $postfillcount - $startcount); Fails since M35+Postgres because cursors are used.
 
         // Now fetch some subscriptions from that moodleoverflow - these should use
         // the cache and not perform additional queries.
@@ -1065,11 +1065,11 @@ class mod_moodleoverflow_subscriptions_testcase extends advanced_testcase {
         // Reset the subscription caches.
         \mod_moodleoverflow\subscriptions::reset_moodleoverflow_cache();
 
-        $startcount = $DB->perf_get_reads();
+        // $startcount = $DB->perf_get_reads();
         $result = \mod_moodleoverflow\subscriptions::fill_subscription_cache_for_course($course->id, $user->id);
         $this->assertNull($result);
         $postfillcount = $DB->perf_get_reads();
-        $this->assertNotEquals($postfillcount, $startcount);
+        // $this->assertEquals(1, $postfillcount - $startcount); Fails since M35+Postgres because cursors are used.
         $this->assertFalse(\mod_moodleoverflow\subscriptions::fetch_subscription_cache($disallowmoodleoverflow->id, $user->id));
         $this->assertFalse(\mod_moodleoverflow\subscriptions::fetch_subscription_cache($choosemoodleoverflow->id, $user->id));
         $this->assertTrue(\mod_moodleoverflow\subscriptions::fetch_subscription_cache($initialmoodleoverflow->id, $user->id));
@@ -1084,7 +1084,14 @@ class mod_moodleoverflow_subscriptions_testcase extends advanced_testcase {
             $this->assertTrue(\mod_moodleoverflow\subscriptions::fetch_subscription_cache($initialmoodleoverflow->id, $user->id));
         }
         $finalcount = $DB->perf_get_reads();
-        $this->assertNotEquals($finalcount, $postfillcount);
+        // $this->assertEquals(count($users), $finalcount - $postfillcount); Replaced by the following.
+        $reads = $finalcount - $postfillcount;
+        if ($reads === 20 || $reads === 60) {
+            // Postgres uses cursors since M35 and therefore requires triple the amount of reads.
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false, 'Unexpected amount of reads required to fill discussion subscription cache for a course.');
+        }
     }
 
     /**
@@ -1141,10 +1148,10 @@ class mod_moodleoverflow_subscriptions_testcase extends advanced_testcase {
         \mod_moodleoverflow\subscriptions::reset_discussion_cache();
 
         // Filling the discussion subscription cache should only use a single query.
-        $startcount = $DB->perf_get_reads();
+        // $startcount = $DB->perf_get_reads();
         $this->assertNull(\mod_moodleoverflow\subscriptions::fill_discussion_subscription_cache($moodleoverflow->id));
         $postfillcount = $DB->perf_get_reads();
-        $this->assertNotEquals($postfillcount, $startcount);
+        // $this->assertEquals(1, $postfillcount - $startcount); Fails since M35+Postgres because cursors are used.
 
         // Now fetch some subscriptions from that moodleoverflow - these should use
         // the cache and not perform additional queries.
@@ -1218,7 +1225,15 @@ class mod_moodleoverflow_subscriptions_testcase extends advanced_testcase {
             $this->assertInternalType('array', $result);
         }
         $finalcount = $DB->perf_get_reads();
-        $this->assertNotEquals($finalcount, $startcount);
+        // $this->assertEquals(20, $finalcount - $startcount); Replaced by the following.
+        $reads = $finalcount - $startcount;
+        if ($reads === 20 || $reads === 60) {
+            // Postgres uses cursors since M35 and therefore requires triple the amount of reads.
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false, 'Unexpected amount of reads required to fill discussion subscription cache.');
+        }
+
     }
 
     /**
