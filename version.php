4e3e17a9b38d58c336e5e9eb900ab7e508280c47
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2018-11-27 14:09:25 +0100

    Bump version no. for release

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_moodleoverflow';
-$plugin->version = 2018112100;
+$plugin->version = 2018112700;
 $plugin->release = 'v3.6-r1';
 $plugin->requires = 2018051703;
 $plugin->maturity = MATURITY_STABLE;
