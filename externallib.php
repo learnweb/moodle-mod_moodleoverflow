3ad95763318cdfee3130060964dfd95473bd2098
Justus Dieckmann <justusdieckmann@wwu.de>
2019-04-04 01:22:10 +0200

    fix reputation not updating

--- externallib.php
+++ externallib.php
@@ -120,6 +120,8 @@ class mod_moodleoverflow_external extends external_api {
             $params['postid'], $params['ratingid'], $cm)) {
             print_error('ratingfailed', 'moodleoverflow');
         }
+
+        $postownerid =  moodleoverflow_get_post_full($params['postid'])->userid;
         $rating      = \mod_moodleoverflow\ratings::moodleoverflow_get_ratings_by_discussion($discussion->id,
             $params['postid']);
         $ownerrating = \mod_moodleoverflow\ratings::moodleoverflow_get_reputation($moodleoverflow->id, $postownerid);
