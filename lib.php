4ae49ca829d17ade32a8c631265832652a152283
Justus Dieckmann <justusdieckmann@wwu.de>
2022-04-13 22:25:16 +0200

    Include icon for Moodle 4.0

--- lib.php
+++ lib.php
@@ -102,6 +102,8 @@ function moodleoverflow_supports($feature) {
             return true;
         case FEATURE_GRADE_HAS_GRADE:
             return true;
+        case FEATURE_MOD_PURPOSE:
+            return MOD_PURPOSE_COLLABORATION;
 
         default:
             return null;
