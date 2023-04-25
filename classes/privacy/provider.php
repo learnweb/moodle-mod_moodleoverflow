7b88e22d129dee21e0f1b95277987e38bd83f4ae
Lars Thoms <SpaceCafe@users.noreply.github.com>
2023-04-25 10:31:32 +0200

    Update classes/privacy/provider.php
    fixes phpunit errors
    
    Co-authored-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -158,7 +158,7 @@ class provider implements
                     SELECT 1 FROM {moodleoverflow_tracking} track WHERE track.moodleoverflowid = mof.id AND track.userid = :tuserid
                 ) OR EXISTS (
                     SELECT 1 FROM {moodleoverflow_grades} g WHERE g.moodleoverflowid = mof.id AND g.userid = :guserid
-                );";
+                )";
 
         $params = [
             'modname'      => 'moodleoverflow',
