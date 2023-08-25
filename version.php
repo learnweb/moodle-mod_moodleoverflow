451417812a92b03f99cfd2e01493f7cb26608b37
Justus Dieckmann <45795270+justusdieckmann@users.noreply.github.com>
2023-08-25 13:37:22 +0200

    Bump version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_moodleoverflow';
-$plugin->version = 2023070300;
+$plugin->version = 2023082500;
 $plugin->release = 'v4.2-r2';
 $plugin->requires = 2020061500; // Requires Moodle 3.9+.
 $plugin->maturity = MATURITY_STABLE;
