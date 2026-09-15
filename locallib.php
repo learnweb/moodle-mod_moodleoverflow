475b443e78fb525e4256bcde0d099dc079ddd172
tamaro <tamaro.walter@uni-muenster.de>
2026-09-15 21:55:33 +0200

    Replace custom sort functions with php usort()

--- locallib.php
+++ locallib.php
@@ -231,58 +231,6 @@ function moodleoverflow_update_all_grades() {
     }
 }
 
-
-/**
- * Function to sort an array with a quicksort algorithm. This function is a recursive function that needs to
- * be called from outside.
- *
- * @param array $array The array to be sorted. It is passed by reference.
- * @param int $low The lowest index of the array. The first call should set it to 0.
- * @param int $high The highest index of the array. The first call should set it to the length of the array - 1.
- *
- * @param string $key The key/attribute after what the algorithm sorts. The key should be an comparable integer.
- * @param string $order The order of the sorting. It can be 'asc' or 'desc'.
- * @return void
- */
-function moodleoverflow_quick_array_sort(&$array, $low, $high, $key, $order) {
-    if ($low >= $high) {
-        return;
-    }
-    $left = $low;
-    $right = $high;
-    $pivot = $array[intval(($low + $high) / 2)]->$key;
-
-    $compare = function ($a, $b) use ($order) {
-        if ($order == 'asc') {
-            return $a < $b;
-        } else {
-            return $a > $b;
-        }
-    };
-
-    do {
-        while ($compare($array[$left]->$key, $pivot)) {
-            $left++;
-        }
-        while ($compare($pivot, $array[$right]->$key)) {
-            $right--;
-        }
-        if ($left <= $right) {
-            $temp = $array[$right];
-            $array[$right] = $array[$left];
-            $array[$left] = $temp;
-            $right--;
-            $left++;
-        }
-    } while ($left <= $right);
-    if ($low < $right) {
-        moodleoverflow_quick_array_sort($array, $low, $right, $key, $order);
-    }
-    if ($high > $left) {
-        moodleoverflow_quick_array_sort($array, $left, $high, $key, $order);
-    }
-}
-
 /**
  * Function to get a record from the database and throw an exception, if the record is not available. The error string is
  * retrieved from moodleoverflow but can be retrieved from the core too.
